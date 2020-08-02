6a910b67f2915ae3f5807a0a2c9288a1a98f0376
prashant.shakya <prashant@essentialaccessibility.com>
2020-08-02 23:32:35 +0530

    First Commit

---
+++ precache-manifest.90cdea275fc92122a4d8bf69d53606da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c248e8e73ddb607dc89662fe368d650",
+    "url": "./index.html"
+  },
+  {
+    "revision": "02e8d7d917e87500dc58",
+    "url": "./static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "fa601b80206939d06f13",
+    "url": "./static/js/2.ab6d49e8.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "./static/js/2.ab6d49e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02e8d7d917e87500dc58",
+    "url": "./static/js/main.2803a1f9.chunk.js"
+  },
+  {
+    "revision": "ef84fb35973d3048646d",
+    "url": "./static/js/runtime-main.0f2917e2.js"
+  }
+]);
