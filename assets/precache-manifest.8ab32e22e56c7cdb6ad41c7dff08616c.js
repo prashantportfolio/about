e13c1ef7d22d676b78070242448562b8622a5fdb
prashant.shakya <prashant@essentialaccessibility.com>
2020-10-03 21:59:30 +0530

    Added new projects

---
+++ assets/precache-manifest.8ab32e22e56c7cdb6ad41c7dff08616c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1be22db19a24c8e2b330b1e2debf204",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0d3cf84bbeb714b914a2",
+    "url": "./static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "fa601b80206939d06f13",
+    "url": "./static/js/2.ab6d49e8.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "./static/js/2.ab6d49e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d3cf84bbeb714b914a2",
+    "url": "./static/js/main.2642d55a.chunk.js"
+  },
+  {
+    "revision": "ef84fb35973d3048646d",
+    "url": "./static/js/runtime-main.0f2917e2.js"
+  }
+]);
